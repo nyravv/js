a828300c51aacd062db1a2fde3a06aff2a44f0b8
Анна Шматова <missis.anika2010@yandex.ru>
2021-01-22 15:45:56 +0300

    change 3.9 Homework Activity 2: ternary operator

--- 03_string-boolean-conditionals/3.9 Homework Activity 2: upper,lower case,substring.js
+++ 03_string-boolean-conditionals/3.9 Homework Activity 2: upper,lower case,substring.js
@@ -1,21 +1,14 @@
 const name = "anNa"
 const surname = "Shmatova"
 
-// вот тут для разнообразия попробовала String.fromCodePoint
-if (name.substr(0, 1) > String.fromCodePoint(90) || name.substr(1) !== name.substr(1).toLowerCase()) { 
-  const newName = name.substr(0, 1).toUpperCase() + name.substr(1).toLowerCase()
-  console.log("Имя было преобразовано: " + newName)
-}
- else {
-  console.log("Имя осталось без изменений: " + name)
- }
+const newName = (name.substr(0, 1) > String.fromCodePoint(90) || name.substr(1) !== name.substr(1).toLowerCase()) ?
+  "Имя было преобразовано: " + name.substr(0, 1).toUpperCase() + name.substr(1).toLowerCase()
+  : "Имя осталось без изменений: " + name
 
+  console.log(newName)
+ 
+const newSurname = (surname.substr(0, 1) !== surname.substr(0, 1).toUpperCase() || surname.substr(1) !== surname.substr(1).toLowerCase()) ?
+"Фамилия была преобразована: "  + surname.substr(0, 1).toUpperCase() + surname.substr(1).toLowerCase()
+  : "Фамилия осталась без изменений: " + surname
 
-if (surname.substr(0, 1) !== surname.substr(0, 1).toUpperCase() || surname.substr(1) !== surname.substr(1).toLowerCase()) {
-  const newSurname = surname.substr(0, 1).toUpperCase() + surname.substr(1).toLowerCase()
-  console.log("Фамилия была преобразована: " + newSurname)
-}
-else {
-  console.log("Фамилия осталась без изменений: " + surname)
-}
-
+  console.log(newSurname)
